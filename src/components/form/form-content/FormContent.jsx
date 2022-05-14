b3282b9d2577eed21c5d4c123e40ef1d92112763
N00bG1rl <403helen@gmail.com>
2022-05-14 11:35:11 +0300

    Form fields created

--- src/components/form/form-content/FormContent.jsx
+++ src/components/form/form-content/FormContent.jsx
@@ -2,37 +2,43 @@ import styles from './FormContent.module.css'
 
 function FormContent() {
 	return (
-		<form action='' className={styles.form}>
-			<h1>Compensation Calculator</h1>
-			<p>
-				<label htmlFor=''>Average income</label>
-				<input
-					id=''
-					type='number'
-					inputMode='numeric'
-					required
-					min='0'
-					max='100000'
-				/>
-				<span>€</span>
+		<form className={styles.formContent}>
+			<h4>
+				Compensation <br /> Calculator
+			</h4>
+			<p className={styles.formInputWrapper}>
+				<label htmlFor='income'>Average income</label>
+				<span>
+					<input
+						id='income'
+						type='number'
+						inputMode='numeric'
+						required
+						min='0'
+						max='100000'
+					/>
+					€
+				</span>
 			</p>
-			<p>
-				<label>Days on sick-leave</label>
-				<input
-					id=''
-					type='number'
-					inputMode='numeric'
-					required
-					min='0'
-					max='240'
-				/>
-				<span>days</span>
+			<p className={styles.formInputWrapper}>
+				<label htmlFor='sick-days'>Days on sick-leave</label>
+				<span>
+					<input
+						id='sick-days'
+						type='number'
+						inputMode='numeric'
+						required
+						min='0'
+						max='240'
+					/>
+					days
+				</span>
 			</p>
-			<p>
+			<p className={styles.checkboxWrapper}>
+				<input type='checkbox' className={styles.checkbox} />
 				<label>I have tubercolosis</label>
-				<input type='checkbox' />
 			</p>
-			<button>Calculate</button>
+			<button className={styles.submitBtn}>Calculate</button>
 		</form>
 	)
 }
