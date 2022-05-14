7eef7ca900ccd42c27930bf56f2ff525f7c09a79
N00bG1rl <403helen@gmail.com>
2022-05-14 13:12:09 +0300

    Form result field styles added

--- src/components/form/form-result/FormResult.jsx
+++ src/components/form/form-result/FormResult.jsx
@@ -3,19 +3,29 @@ import styles from './FormResult.module.css'
 function FormResult() {
 	return (
 		<article className={styles.formResult}>
-			<section>
-				<p>The employer compensate 4 days</p>
-				<p>112,00€</p>
-				<p>Daily allowance 28.00 €</p>
-			</section>
-			<section>
-				<p>Health Insurence compensate 0 days</p>
-				<p>0,00€</p>
-				<p>Daily allowance 28.00 €</p>
-			</section>
-			<section>
-				<h3>Compensation total for 7 days (net)</h3>
-				<p>112,00€</p>
+			<article className={styles.overview}>
+				<section>
+					<p>
+						The employer compensates <br /> <b>4 days</b>
+					</p>
+					<p className={styles.resultSum}>112,00€</p>
+					<p className={styles.dailySum}>
+						Daily allowance <br /> 28,00 €
+					</p>
+				</section>
+				<section>
+					<p>
+						Health Insurance compensates <br /> <b>0 days</b>
+					</p>
+					<p className={styles.resultSum}>0,00€</p>
+					<p className={styles.dailySum}>
+						Daily allowance <br /> 28,00 €
+					</p>
+				</section>
+			</article>
+			<section className={styles.totalAmount}>
+				<p>Compensation total for 7 days (net)</p>
+				<p className={styles.totalSum}>112,00€</p>
 			</section>
 		</article>
 	)
